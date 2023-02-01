79cabcb7fbc992552d8c5474f5080e0a3bd6966a
rouna <godemode12@gmail.com>
2023-02-01 15:50:48 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div>
+          <form action="">
+            <label htmlFor="">Name :</label>
+            <input type="text"/>
+            <label htmlFor="">Email :</label>
+            <input type="email"/>
+            <label htmlFor="">Password :</label>
+            <input type="text"/>
+          </form>
+        </div>
+    );
+};
 
 export default App;
